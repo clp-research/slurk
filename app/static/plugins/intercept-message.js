f0693a39dcf2d7ee896716bc68ddf69e364e4fef
Tim <t.diekmann.3dv@gmail.com>
2018-10-11 18:05:45 +0200

    Remove room parameter from submitting

--- app/static/plugins/intercept-message.js
+++ app/static/plugins/intercept-message.js
@@ -1,7 +1,7 @@
 if (text.match("^/")) {
-    submit_command(current_room, text.substr(1).split(' '));
+    submit_command(text.substr(1).split(' '));
     display_message(current_user, current_timestamp, text.substr(1), true);
 } else {
-    submit_command(current_room, ["intercept", text]);
+    submit_command(["intercept", text]);
     display_message(current_user, current_timestamp, text, false);
 }
--- app/static/plugins/send-message.js
+++ app/static/plugins/send-message.js
@@ -1,7 +1,16 @@
 if (text.match("^/")) {
-    submit_command(current_room, text.substr(1).split(' '));
+    // parameter: [string]
+    submit_command(text.substr(1).split(' '));
+    // user: dict, timestamp: int, text: string, private: bool
     display_message(current_user, current_timestamp, text.substr(1), true);
+} else if (text.match("^image:")) {
+    // image: string, width: int, height: int
+    submit_image(text.substr(6), 300, 300);
+    // user: dict, timestamp: int, image: string, width: int, height: int, private: bool
+    display_image(current_user, current_timestamp, text.substr(6), 300, 300, false);
 } else {
-    submit_command(current_room, ["intercept", text]);
+    // text: string
+    submit_command(["intercept", text]);
+    // user: dict, timestamp: int, text: string, private: bool
     display_message(current_user, current_timestamp, text, false);
 }
