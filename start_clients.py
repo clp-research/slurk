4c458e890ba0549b1797bc902eca635efd40b9c1
Simeon Schüz <simeon.schuez@uni-bielefeld.de>
2019-02-08 15:27:36 +0100

    osx support

--- start_clients.py
+++ start_clients.py
@@ -2,27 +2,52 @@ import random
 import requests
 import webbrowser
 import lxml.html
+import configparser
+import sys
+from time import sleep
 
-link = []
+platform = sys.platform
 
-def insert_names_and_tokens():
-    for i in ["Dr. John A. Zoidberg"]:#, "Professor Farnsworth"]:
-        full_name = i
+if platform == 'darwin':
+    import appscript
+    CHROME_PATH = 'open -a /Applications/Google\ Chrome.app %s'
+    FIREFOX_PATH = 'open -a /Applications/Firefox.app %s'
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+secret_key = config['server']['secret-key']
+
+def get_client_links(*names, key):
+    """ return links to log in as a client """
+    links= []
+    for i in names:
+        name = i
         url = 'http://127.0.0.1:5000/token'
         s = requests.session()
         r = s.get(url)
         source = lxml.html.document_fromstring(r.content)
         token = source.xpath('//input[@name="csrf_token"]/@value')[0]
         headers = {'Referer': 'http://127.0.0.1:5000/token'}
-        data = {'csrf_token': token, 'room': '1', 'task': '1', 'source': '{}'.format(full_name)}
+        data = {'csrf_token': token, 'room': '1', 'task': '1', 'source': name, 'key': key}
         login_token = s.post(url, data=data, headers=headers).text
         if login_token.endswith('<br />'):
             login_token = login_token[:-6]
-        uris = 'http://127.0.0.1:5000/?name={}&token={}'.format(full_name, login_token)
-        link.append(uris)
-    return link
+        uris = 'http://127.0.0.1:5000/?name={}&token={}'.format(name, login_token)
+        links.append(uris)
+    return links
 
-links = insert_names_and_tokens()
+if __name__ == "__main__":
 
-webbrowser.get('chromium-browser').open(links[0])
-#webbrowser.get('firefox').open(links[1])
+    # get the links
+    links =  get_client_links('Dr. John Zoidberg', 'Professor Farnsworth', key=secret_key)
+
+    # log in using firefox (linux) or chrome (mac)
+    for link in links:
+        if sys.platform == 'linux':
+            webbrowser.get('firefox').open(link)
+        elif platform == 'darwin':
+            webbrowser.get(CHROME_PATH).open(link)
+        else:
+            print ('Could not detect operating system')
+            break
+        sleep(2)
--- start_server.py
+++ start_server.py
@@ -1,27 +1,38 @@
 import os
+import sys
 import argparse
 import requests
 import lxml.html
+import configparser
 from time import sleep
+
+platform = sys.platform
+
+if platform == 'darwin':
+    import appscript
 
 parser = argparse.ArgumentParser()
 parser.add_argument('bot', nargs='*')
-parser.add_argument('--terminal', default="gnome-terminal")
 args = parser.parse_args()
 
-terminal = args.terminal
+terminal = "gnome-terminal"
 bots = args.bot
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dir_virtualenv = "/home/simeon/my_virtualenv/bin"
 
-def get_bot_token(name):
+config = configparser.ConfigParser()
+config.read('config.ini')
+secret_key = config['server']['secret-key']
+
+def get_bot_token(name, key):
+    """ get a token for connecting a bot """
     url = 'http://127.0.0.1:5000/token'
     s = requests.session()
     r = s.get(url)
     source = lxml.html.document_fromstring(r.content)
     token = source.xpath('//input[@name="csrf_token"]/@value')[0]
     headers = {'Referer': 'http://127.0.0.1:5000/token'}
-    data = {'csrf_token': token, 'room': '1', 'task': '2', 'reusable': 'y', 'source': '{}'.format(name)}
+    data = {'csrf_token': token, 'room': '1', 'task': '2', 'reusable': 'y', 'source': name, 'key': key}
     login_token = s.post(url, data=data, headers=headers).text
     if login_token.endswith('<br />'):
         login_token = login_token[:-6]
@@ -29,19 +40,32 @@ def get_bot_token(name):
 
 if __name__ == "__main__":
 
+    # print basic information
     print ("Directory:",dir_path)
     print ("Terminal:",terminal)
     print ("Bots: ",bots)
 
+    # start slurk in new shell session
     print ("\nstarting chat server")
-    os.system('{trmnl} -x bash -c "source {virtualenv}/activate; python3.6 {path}/chat.py"'.format(virtualenv=dir_virtualenv, trmnl=terminal, path=dir_path))
-    print ("success")
-    sleep(1)
+    if platform == 'linux':
+        os.system('{trmnl} -x bash -c "source {virtualenv}/activate; python3.6 {path}/chat.py"'.format(virtualenv=dir_virtualenv, trmnl=terminal, path=dir_path))
+        print ("success")
+    elif platform == 'darwin':
+        appscript.app('Terminal').do_script('source activate {virtualenv}; python3.6 {path}/chat.py'.format(virtualenv=dir_virtualenv, path=dir_path))
+        print ("success")
+    else:
+        print ('Could not detect operating system')
+    sleep(2)
 
+    # start bots in new shell sessions
     for i in args.bot:
-        token = get_bot_token(i)
+        token = get_bot_token(i, secret_key)
         print ("\n\nstarting", i, "\ntoken:", token)
-        os.system('{trmnl} -x bash -c "source {virtualenv}/activate; python3.6 {path}/{name} {bot_token} " --title={name}'.format(virtualenv=dir_virtualenv, trmnl=terminal, path=dir_path, name=i, bot_token=token))
+        if platform == 'linux':
+            os.system('{trmnl} -x bash -c "source {virtualenv}/activate; python3.6 {path}/{name} {bot_token} " --title={name}'.format(virtualenv=dir_virtualenv, trmnl=terminal, path=dir_path, name=i, bot_token=token))
+        elif platform == 'darwin':
+            appscript.app('Terminal').do_script('source activate {virtualenv}; python3.6 {path}/{name} {bot_token}'.format(virtualenv=dir_virtualenv, path=dir_path, name=i, bot_token=token))
+        else:
+            print ('Could not detect operating system')
+            break
         sleep(1)
-
-# os.system('gnome-terminal -x bash -c "source activate_virtualenv.sh;python3.6 chat.py read -n1"')
