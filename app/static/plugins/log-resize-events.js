fac340b765d03ce7b7468f5e74a114245a12ca39
Tim Diekmann <tim.diekmann@3dvision.de>
2019-02-26 14:05:27 +0100

    feat(client): Add script for logging window resize events
    Closes #19

---
+++ app/static/plugins/log-resize-events.js
@@ -0,0 +1,12 @@
+function log_resize_event() {
+    socket.emit('log', { room: self_room, type: "resize", width: $(window).width(), height: $(window).height() });
+};
+
+var resizeTimer;
+$(window).resize(function () {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(log_resize_event, 500);
+});
+
+// Initial size of window. Otherwise it's hard to track changes
+log_resize_event()
