7a97e82a5c576e9e932603309fdc3ef2be553997
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2019-03-22 13:28:26 +0100

    Revert "Attempt to fix calling `visit_reference`"
    This reverts commit 8903497b303cf2bb373dbaf11671407edef3f821.

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ class PatchedHTMLTranslator(HTMLTranslator):
     def visit_reference(self, node):
         if node.get('newtab') or not (node.get('target') or node.get('internal') or 'refuri' not in node):
             node['target'] = '_blank'
-        super.visit_reference(node)
+        super().visit_reference(node)
 
 def setup(app):
     app.set_translator('html', PatchedHTMLTranslator)
