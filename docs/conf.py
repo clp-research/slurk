2187efc5bc319609748d86e48f3318d811d2a340
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2019-03-22 13:38:23 +0100

    Open external links in new tab (#57)
    * feat(docs): Open external links in new tab
    
    fixes #7
    
    * Attempt to fix calling `visit_reference`
    
    * Use python 3.7 in travis
    
    * Use *xenial* as distribution on Travis
    
    * Revert "Attempt to fix calling `visit_reference`"
    
    This reverts commit 8903497b303cf2bb373dbaf11671407edef3f821.

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,18 @@
 
 from subprocess import Popen, PIPE
 
+from sphinx.writers.html import HTMLTranslator
+
+
+class PatchedHTMLTranslator(HTMLTranslator):
+    def visit_reference(self, node):
+        if node.get('newtab') or not (node.get('target') or node.get('internal') or 'refuri' not in node):
+            node['target'] = '_blank'
+        super().visit_reference(node)
+
+def setup(app):
+    app.set_translator('html', PatchedHTMLTranslator)
+
 def get_version():
     """
     Returns project version as string from 'git describe' command.
