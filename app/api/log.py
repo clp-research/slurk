09ff3532e891146e5c7ba08847bb3434f852cacc
soerenetler <soeren.etler@gmail.com>
2019-08-26 11:44:52 +0200

    Add log event (#85)
    Add log event

--- app/api/log.py
+++ app/api/log.py
@@ -1,6 +1,13 @@
 import bson
 from logging import getLogger
 
+from ..models.room import Room
+from ..models.user import User
+
+from .. import socketio
+
+from flask_login import login_required, current_user
+
 
 def log_event(event, user, room=None, data=None):
     from .. import db, Log
@@ -21,3 +28,22 @@ def log_event(event, user, room=None, data=None):
     db.session.add(log)
     db.session.commit()
     return log
+
+@socketio.on('log')
+@login_required
+def log(data):
+    sender = current_user if 'sender_id' not in data else User.query.get(data['sender_id'])
+    if not sender:
+        return False, "sender not found"
+
+    if 'room' in data:
+        room = Room.query.get(data['room'])
+        if not room:
+            return False, "room not found"
+
+    reduced_data = data
+    if 'type' in reduced_data: del reduced_data['type']
+    if 'room' in reduced_data: del reduced_data['room']
+    if 'sender_id' in reduced_data: del reduced_data['sender_id']
+
+    log_event(data["type"], current_user, room=room, data=reduced_data)
