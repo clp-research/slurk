e408bf63f5ea70bd7e69a464f117610eabd14d78
Soeren Etler <soeren.etler@gmail.com>
2019-08-26 11:37:49 +0200

    add whole data object to log event

--- app/api/log.py
+++ app/api/log.py
@@ -32,6 +32,7 @@ def log_event(event, user, room=None, data=None):
 @socketio.on('log')
 @login_required
 def log(data):
+    
     sender = current_user if 'sender_id' not in data else User.query.get(data['sender_id'])
     if not sender:
         return False, "sender not found"
@@ -41,4 +42,9 @@ def log(data):
         if not room:
             return False, "room not found"
 
-    log_event(data["type"], current_user, room=room, data={'message':data['message']})
+    reduced_data = data
+    if 'type' in data: del data['type']
+    if 'room' in data: del data['room']
+    if 'sender_id' in data: del data['sender_id']
+
+    log_event(data["type"], current_user, room=room, data=reduced_data)
