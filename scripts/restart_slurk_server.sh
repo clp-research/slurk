d5e23660c553a30ab3db402a2423b1fd9a88533f
Patrick Kahardipraja <patrick.khdipraja@gmail.com>
2020-05-05 10:56:57 +0200

    changed port in scripts/restart_slurk_server.sh to 80

--- scripts/restart_slurk_server.sh
+++ scripts/restart_slurk_server.sh
@@ -7,5 +7,5 @@ sudo docker kill slurky
 sudo docker rm slurky
 
 # start docker container for slurk server
-SLURK_SERVER_ID=$(docker run --name=slurky -p $PORT:5000 -e SECRET_KEY=your-key -d slurk/server)
+export SLURK_SERVER_ID=$(docker run --name=slurky -p 80:5000 -e SECRET_KEY=your-key -d slurk/server)
 sleep 1
