f45f1c9115edf9af6e33f72659efda14f3355e1f
Jana Götze <jana.goetze@uni-potsdam.de>
2020-05-06 18:02:51 +0200

    add several ways to create tokens

--- scripts/create_token.sh
+++ scripts/create_token.sh
@@ -9,19 +9,30 @@ ROOM=$2
 if [ "$#" -eq 2 ]
 then
   curl -X POST \
-         -H "Authorization: Token $ADMIN" \
-         -H "Content-Type: application/json" \
-         -H "Accept: application/json" \
-         -d "{\"room\": \"$ROOM\", \"message_text\": true}" \
-         localhost/api/v2/token | sed 's/^"\(.*\)"$/\1/'
+        -H "Authorization: Token $ADMIN" \
+        -H "Content-Type: application/json" \
+        -H "Accept: application/json" \
+        -d "{\"room\": \"$ROOM\", \"message_text\": true}" \
+        localhost/api/v2/token | sed 's/^"\(.*\)"$/\1/'
+
 elif [ "$#" -eq 3 ] && [ $3 == '--concierge' ]
 then
   curl -X POST \
-         -H "Authorization: Token $ADMIN" \
-         -H "Content-Type: application/json" \
-         -H "Accept: application/json" \
-         -d "{\"room\": \"$ROOM\", \"message_text\": true, \"room_create\": true, \"user_room_join\": true, \"user_room_leave\": true}" \
-         localhost/api/v2/token | sed 's/^"\(.*\)"$/\1/'
+        -H "Authorization: Token $ADMIN" \
+        -H "Content-Type: application/json" \
+        -H "Accept: application/json" \
+        -d "{\"room\": \"$ROOM\", \"message_text\": true, \"room_create\": true, \"user_room_join\": true, \"user_room_leave\": true}" \
+        localhost/api/v2/token | sed 's/^"\(.*\)"$/\1/'
+
+elif [ "$#" -eq 3 ] && [ $3 == '--echo' ]
+then
+  curl -X POST \
+        -H "Authorization: Token $ADMIN_TOKEN" \
+        -H "Content-Type: application/json" \
+        -H "Accept: application/json" \
+        -d '{"room": "waiting_room", "message_text": true, "user_room_join": true}' \
+        localhost/api/v2/token | sed 's/^"\(.*\)"$/\1/'
+
 else
   echo "You need to specify the authorization token and a room identifier"
   echo "Ex: 'sh $0 \$ADMIN_TOKEN test_room'"
