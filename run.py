beb0254d1439a543827b47999732983726b7fd97
Tim Diekmann <tim.diekmann@3dvision.de>
2021-05-25 01:14:11 +0200

    Fix running app from Docker

--- run.py
+++ run.py
@@ -3,14 +3,16 @@ from gevent import monkey
 
 monkey.patch_all(subprocess=True)  # NOQA
 
-from app import create_app, socketio
+from app import create_app
 
+
+host = os.environ.get('HOST', '0.0.0.0')
+port = int(os.environ.get('PORT', 5000))
+
+app = create_app()
 
 if __name__ == '__main__':
-    host = os.environ.get('HOST', '0.0.0.0')
-    port = int(os.environ.get('PORT', 5000))
-
-    app = create_app()
+    from app import socketio
     socketio.run(
         app,
         host,
