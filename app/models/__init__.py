23ba12cecd0da26d78b3a68620b2ae2b2dd4af59
Tim Diekmann <tim.diekmann@3dvision.de>
2021-06-06 16:09:52 +0200

    Remove duplicated code

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,3 @@
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
-
 from .layout import Layout
 from .log import Log
 from .room import Room
@@ -8,10 +5,3 @@ from .task import Task
 from .token import Token
 from .user import User
 from .permissions import Permissions
-
-
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
