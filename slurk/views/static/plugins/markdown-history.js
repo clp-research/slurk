babd8c0d07ccd5925ddcc9cca4d308e1b925fe18
Tim Diekmann <tim.diekmann@3dvision.de>
2021-07-20 18:02:43 +0200

    Fix time in markdown history

--- slurk/views/static/plugins/markdown-history.js
+++ slurk/views/static/plugins/markdown-history.js
@@ -1,15 +1,10 @@
-if (element.date_modified !== null)
-    date = element.date_modified
-else
-    date = element.date_created
-
 if (element.event === 'text_message') {
     if (element.data.html) {
         $.getScript('https://cdn.jsdelivr.net/npm/showdown@1.9.0/dist/showdown.min.js', () => {
             const converter = new showdown.Converter();
             display_message(
                 element.user,
-                date,
+                element.date_modified !== null ? element.date_modified : element.date_created,,
                 converter.makeHtml(element.data.message),
                 element.receiver !== null,
                 true);
@@ -17,14 +12,14 @@ if (element.event === 'text_message') {
     } else {
         display_message(
             element.user,
-            date,
+            element.date_modified !== null ? element.date_modified : element.date_created,,
             element.data.message,
             element.receiver !== null);
     }
 } else if (element.event === "image_message") {
     display_image(
         element.user,
-        date,
+        element.date_modified !== null ? element.date_modified : element.date_created,,
         element.data.url,
         element.data.width,
         element.data.height,
