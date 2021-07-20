77655b7d15fc348168ab220fe131f123ed6f63e7
Tim Diekmann <tim.diekmann@3dvision.de>
2021-07-20 23:33:27 +0200

    Revert "Fix time in markdown history"
    This reverts commit babd8c0d07ccd5925ddcc9cca4d308e1b925fe18.

--- slurk/views/static/plugins/markdown-history.js
+++ slurk/views/static/plugins/markdown-history.js
@@ -1,22 +1,27 @@
+if (element.date_modified !== null)
+    date = element.date_modified
+else
+    date = element.date_created
+
 if (element.event === 'text_message') {
     if (element.data.html) {
         display_message(
             element.user,
-            element.date_modified !== null ? element.date_modified : element.date_created,
+            date,
             markdown.makeHtml(element.data.message),
             element.receiver !== null,
             true);
     } else {
         display_message(
             element.user,
-            element.date_modified !== null ? element.date_modified : element.date_created,
+            date,
             element.data.message,
             element.receiver !== null);
     }
 } else if (element.event === "image_message") {
     display_image(
         element.user,
-        element.date_modified !== null ? element.date_modified : element.date_created,
+        date,
         element.data.url,
         element.data.width,
         element.data.height,
