9a485d9bbad71ac122ca8768cd4737bf514e3e9c
Wencke Liermann <wencke.liermann@uni-potsdam.de>
2021-07-25 13:45:55 +0200

    Add script to create user and apply suggestions

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -2,7 +2,7 @@
 set -eu
 
 # Parameters:
-#   scripts/create_room_token.sh room_id permissions_path [registrations_left] [task_id]
+#   scripts/create_user.sh name token_id
 # Environment variables:
 #   SLURK_TOKEN: Token to pass as authorization, defaults to `00000000-0000-0000-0000-000000000000`
 #   SLURK_HOST: Host name to use for the request, defaults to `http://localhost`
@@ -13,16 +13,13 @@ HOST=${SLURK_HOST:-http://localhost}
 PORT=${SLURK_PORT:-5000}
 
 if [ "$#" -lt 2 ]; then
-  echo "Usage: $0 room_id permissions_path [registrations_left] [task_id]" 1>&2
-  echo "For more info see $HOST:$PORT/rapidoc#post-/slurk/api/tokens"
-  exit 1
+    echo "Usage: $0 name token_id" 1>&2
+    echo "For more info see $HOST:$PORT/rapidoc#post-/slurk/api/users"
+    exit 1
 fi
 
-ROOM=$1
-REGISTRATIONS=${3:-1}
-TASK=${4:-null}
-
-permissions_path=$2
+NAME=$1
+USER_TOKEN=$2
 
 function check_error {
     if [ "$(jq '. | has("code")' <<< "$1")" = true ]; then
@@ -34,19 +31,11 @@ function check_error {
     fi
 }
 
-response=$(curl -sX POST $HOST:$PORT/slurk/api/permissions \
-    -H "Accept: application/json" \
-    -H "Authorization: Bearer $TOKEN" \
-    -H "Content-Type: application/json" \
-    -d @$permissions_path)
-check_error "$response"
-permissions=$(echo "$response" | jq .id)
-
 response=$(curl -sX POST \
     -H "Authorization: Bearer $TOKEN" \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
-    -d "{\"permissions_id\": $permissions, \"room_id\": $ROOM, \"registrations_left\": $REGISTRATIONS, \"task_id\": $TASK}" \
-    $HOST:$PORT/slurk/api/tokens)
+    -d "{\"name\": \"$NAME\",\"token_id\": \"$USER_TOKEN\"}" \
+    $HOST:$PORT/slurk/api/users)
 check_error "$response"
 echo "$response" | jq
