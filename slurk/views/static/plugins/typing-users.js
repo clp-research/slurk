6972091ef8e510023816607c5382a5496547209c
Wencke Liermann <wencke.liermann@uni-potsdam.de>
2022-01-08 00:58:08 +0100

    Integrate placeholder for typing-users plugin into chat layout

--- slurk/views/static/plugins/typing-users.js
+++ slurk/views/static/plugins/typing-users.js
@@ -1,11 +1,27 @@
 let typing_users = Object.values(users);
 
+let info_text = $(
+    "<li class='other'>" +
+    "  <div class='message-box'>" +
+    "    <div class='dot-flashing'></div>" +
+    "    <span class='message'></span>" +
+    "  </div>" +
+    "</li>");
+
+
 if (typing_users.length === 0) {
     $("#typing").text("");
-} else if (typing_users.length === 1) {
-    $("#typing").html("<em>" + typing_users[0] + "</em> is typing ...");
-} else if (typing_users.length === 2) {
-    $("#typing").html("<em>" + typing_users.join("</em> and <em>") + "</em> are typing ...");
 } else {
-    $("#typing").html("<em>" + typing_users.join("</em>, <em>") + "</em> are typing ...");
+    if (typing_users.length === 1) {
+        info_text.find(".message").html("<em>" + typing_users[0] + "</em> is typing ...");
+    } else if (typing_users.length === 2) {
+        info_text.find(".message").html("<em>" + typing_users.join("</em> and <em>") + "</em> are typing ...");
+    } else {
+        info_text.find(".message").html("<em>" + typing_users.join("</em>, <em>") + "</em> are typing ...");
+    }
+    $("#typing").html(info_text);
+
+    let content = $('#content');
+    content.animate({ scrollTop: content.prop("scrollHeight") }, 0);
 }
+
