ba34a7f48a6e609057c386422bbe7443131e78d2
seba <seba.g90@gmail.com>
2023-05-03 10:09:34 +0200

    use scope defined variables

--- slurk/views/static/plugins/collapsible-task-instructions.js
+++ slurk/views/static/plugins/collapsible-task-instructions.js
@@ -1,11 +1,11 @@
 // https://www.w3schools.com/howto/howto_js_collapsible.asp
 
-var coll = document.getElementsByClassName("collapsible");
+let coll = document.getElementsByClassName("collapsible");
 
 for (let i=0; i<coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
