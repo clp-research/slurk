34470d8b87709dc9bf8d2a1d646c68f26079fcc7
seba <seba.g90@gmail.com>
2023-05-03 10:06:22 +0200

    use scope defined index variable

--- slurk/views/static/plugins/collapsible-task-instructions.js
+++ slurk/views/static/plugins/collapsible-task-instructions.js
@@ -1,9 +1,8 @@
 // https://www.w3schools.com/howto/howto_js_collapsible.asp
 
 var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
+for (let i=0; i<coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
